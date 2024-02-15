b856190056a674bc91185a4b6accf33f051c8b88
shuguruiua <popov.yk@gmail.com>
2024-02-15 14:38:10 +0200

    Update: main_2.py

--- main_2.py
+++ main_2.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import logging
 
 from sqlalchemy import func, desc, select, and_, distinct
+from sqlalchemy.exc import SQLAlchemyError
 
 import argparse
 import random
@@ -35,20 +36,21 @@ def random_date(start_date, end_date, result=None):
 
 
 def create_record(model, name):
+
     if model == "Grade":
-        student_ids = session.scalars(select(Student.id)).all()
-        subject_ids = session.scalars(select(Subject.id)).all()
+        student_ids = session.query(Student.id).all()
+        subject_ids = session.query(Subject.id).all()
         model_name = eval(model)(
             grade=name,
-            student_id=random.choice(student_ids),
-            subject_id=random.choice(subject_ids),
+            student_id=random.choice(student_ids)[0],
+            subject_id=random.choice(subject_ids)[0],
             grade_date=random_date('2022.09.15', '2023.05.01')
         )
         session.add(model_name)
         session.commit()
-        subqery = session.query(func.max(Grade.id)).scalar()
+        subquery = session.query(func.max(Grade.id)).scalar()
         query_res = session.query(Grade.grade, Grade.grade_date, Student.fullname, Subject.name) \
-            .select_from(Grade).join(Student).join(Subject).filter(Grade.id == subqery).first()
+            .select_from(Grade).join(Student).join(Subject).filter(Grade.id == subquery).first()
 
         a, b, c, d = query_res
         logging.info(f"Grade: {a} | Grade date: {b} | Student: {c} | Subject: {d}")
@@ -62,10 +64,10 @@ def create_record(model, name):
         session.commit()
         return print(f'New {model} successfully added to table {model.lower()}s with name "{name}"')
     elif model == "Student":
-        group_ids = session.scalars(select(Group.id)).all()
+        group_ids = session.query(Group.id).all()
         model_name = eval(model)(
             fullname=name,
-            group_id=random.choice(group_ids)
+            group_id=random.choice(group_ids)[0]
         )
         session.add(model_name)
         session.commit()
@@ -80,10 +82,10 @@ def create_record(model, name):
         return print(f'New {model} successfully added to table {model.lower()}s with name "{name}"')
 
     elif model == "Subject":
-        teacher_ids = session.scalars(select(Teacher.id)).all()
+        teacher_ids = session.query(Teacher.id).all()
         model_name = eval(model)(
             name=name,
-            teacher_id=random.choice(teacher_ids)
+            teacher_id=random.choice(teacher_ids)[0]
         )
         session.add(model_name)
         session.commit()
@@ -104,25 +106,27 @@ def list_records(model):
 
 
 def update_record(model, ids, name):
-    if model in ["Student", "Teacher"]:
-        model_update = session.query(eval(model)).get(int(ids))
-        model_update.fullname = name
-        session.add(model_update)
-        session.commit()
-    elif model == "Grade":
-        model_update = session.query(eval(model)).get(ids)
-        model_update.grade = name
-        session.add(model_update)
-        session.commit()
-    else:
-        model_update = session.query(eval(model)).get(ids)
-        model_update.name = name
-        session.add(model_update)
-        session.commit()
+    model_class = eval(model)
+    model_update = session.get(model_class, ids)
+    if model_update:
 
+        if model in ["Student", "Teacher"]:
+            model_update.fullname = name
+        elif model == "Grade":
+            model_update.grade = name
+        else:
+            model_update.name = name
+        try:
+            session.add(model_update)
+            logging.info(f"Table: {model.lower()}s with ID: {ids} was successfully updated with data: {name}")
+        except SQLAlchemyError as e:
+            print("Ooops...", e)
+            session.rollback()
+        finally:
+            session.commit()
 
 def remove_record(model, ids):
-    model_remove = session.query(eval(model)).get(ids)
+    model_remove = session.get(eval(model), ids)
     session.delete(model_remove)
     session.commit()
 
